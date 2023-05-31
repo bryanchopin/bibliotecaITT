3f2bec6311bc7592e679d036beaa0b8b0f8bb269
Brychxpin <59489846+bryanchopin@users.noreply.github.com>
2023-05-31 16:37:02 -0700

    Fix nextUath problem

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,18 +1,18 @@
-import NextAuth from 'next-auth';
-import Providers from 'next-auth/providers';
+// import NextAuth from 'next-auth';
+// import Providers from 'next-auth/providers';
 
-const providers = [
-  Providers.Google({
-    clientId: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  }),
-];
+// const providers = [
+//   Providers.Google({
+//     clientId: process.env.GOOGLE_CLIENT_ID,
+//     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//   }),
+// ];
 
-const callbacks = {};
+// const callbacks = {};
 
-const options = {
-  providers,
-  callbacks,
-};
+// const options = {
+//   providers,
+//   callbacks,
+// };
 
-export default (req, res) => NextAuth(req, res, options);
+// export default (req, res) => NextAuth(req, res, options);
